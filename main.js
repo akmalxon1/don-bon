f0eeda0ad0bc8d0bc12ac96e6f573a037bc4b6ea
akmalxon1 <akmalxon359@gmail.com>
2024-08-20 12:03:00 +0500

    hello js

---
+++ main.js
@@ -0,0 +1,24 @@
+let body = document.querySelector('body');
+
+let div = document.createElement('div');
+
+
+
+body.append(div);
+
+
+
+let c = prompt('Ismingizni kiriting');
+div.append(c);
+
+div.style.color = 'red';
+div.style.fontSize = '70px';
+div.style.textAlign = 'center';
+div.style.textTransform = 'uppercase';
+div.style.fontWeight = 'bold';
+div.style.fontFamily = 'cursive';
+div.style.textShadow = '0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #0ff, 0 0 70px #0ff, 0 0 80px #0ff, 0 0 100px #0ff, 0 0 150px #0ff';
+div.style.padding = '250px';
+div.style.display = 'flex';
+div.style.justifyContent = 'center';
+div.style.alignItems = 'center';
